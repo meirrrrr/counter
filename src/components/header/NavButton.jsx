8dc2430a4562ed183b235b88e04eb10f17f04193
meir <meiirzhan04@gmail.com>
2024-02-02 14:24:27 +0600

    Updates

--- src/components/header/NavButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function NavButton({ children, onClick, isActive }) {
-  return (
-    <button className={isActive ? "button active" : "button"} onClick={onClick}>
-      {children}
-    </button>
-  );
-}
